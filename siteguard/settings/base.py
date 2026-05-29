fbe0ea16065cdde46c90f39b54850b1dbf0fbc56
Your Name <your-email@example.com>
2026-05-29 14:49:48 +0530

    Final certification pass before v3.1.0

--- siteguard/settings/base.py
+++ siteguard/settings/base.py
@@ -95,6 +95,7 @@ TEMPLATES = [
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
+                "django.template.context_processors.static",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "monitor.context_processors.global_ui_context",
